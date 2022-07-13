5fa81ff214f824c04ffe3d2be1df443bd636672f
arunp3441 <arun.p3441@gmail.com>
2022-07-13 17:29:22 +0530

    initial commit

---
+++ src/main/java/io/arunp/betterreadsapp/book/BookController.java
@@ -0,0 +1,34 @@
+package io.arunp.betterreadsapp.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    private final String COVER_IMAGE_ROOT = "https://covers.openlibrary.org/b/id/";
+
+    @GetMapping(value="/books/{bookId}")
+    private String getBook(@PathVariable String bookId, Model model){
+        Optional<Book> optionalBook = bookRepository.findById(bookId);
+        if(optionalBook.isPresent()){
+            Book book = optionalBook.get();
+            String coverImage = "/images/no-images.jpg";
+            if(book.getCoverIds() != null && !book.getCoverIds().isEmpty()){
+                coverImage = COVER_IMAGE_ROOT+book.getCoverIds().get(0)+"-L.jpg";
+            }
+            model.addAttribute("coverImage",coverImage);
+            model.addAttribute("book",book);
+            return "book";
+        }
+        return "book-not-found";
+    }
+}
